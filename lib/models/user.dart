125b47810a7f432b4c625c11a65b8c81c9cdff02
ZHEBINNG <210886@student.upm.edu.my>
2023-05-27 14:46:59 +0800

    FIRST COMMIT

---
+++ lib/models/user.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+
+class MyUser {
+  final String? uid;
+
+  MyUser({this.uid});
+}
+
+class UserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserData(
+      {required this.uid,
+      required this.sugars,
+      required this.strength,
+      required this.name});
+}
